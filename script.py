2e11446dba9b32b121d8e3c4303dd153af58a939
unknown <shusei,yahata.30@gmail.com>
2021-12-03 01:35:56 +0900

    [mod]取得項目の修正

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import csv
+import regex
 
 shop_list = []
 url_list = []
@@ -12,33 +13,37 @@ headers = {
 }
 
 # url_address = "https://www.homes.co.jp/chintai/tokyo/list/?cond%5Broseneki%5D%5B88205061%5D=88205061&cond%5Bmonthmoneyroomh%5D=0&cond%5Bhousearea%5D=0&cond%5Bhouseageh%5D=0&cond%5Bwalkminutesh%5D=0&bukken_attr%5Bcategory%5D=chintai&bukken_attr%5Bpref%5D=13"
-url_address = "https://www.tokyodisneyresort.jp/tdl/restaurant/detail/317/"
+url_address = "https://www.tokyodisneyresort.jp/tdl/restaurant.html"
 
 session = requests.Session()
 html = session.get(url_address, headers=headers)
 soup = BeautifulSoup(html.text, "html.parser")
 
-for table in soup.findAll(class_="link"):
-    for sub in table.findAll("li"):
+for table in soup.findAll(class_="section-list"):
+
+    # 店名の取得
+    for sub in table.findAll(class_="heading3"):
+        shop_name = re.sub(r"^\s+|\s+$", "", sub.text)
+
+        # リストに追加
+        shop_list.append(shop_name)
+
+    # URLの取得
+    for sub in table.findAll(class_="button"):
         for parts in sub.findAll("a"):
-            
-            # 店名の取得
-            shop_name = re.sub(r"^\s+|\s+$", "", parts.text)
+            if regex.findall("店舗メニュー",parts.text):
+                
+                url = parts.get("href")
+                url = "https://www.tokyodisneyresort.jp/" + url
 
-            # URLの取得
-            url = parts.get("href")
-            url = "https://www.tokyodisneyresort.jp/" + url
-
-            # 配列に追加
-            shop_list.append(shop_name)
-            url_list.append(url)
+                # リストに追加
+                url_list.append(url)
 
 all_list = list(zip(
     shop_list,
     url_list,
 ))
 
-# print(all_list)
 
 # csvファイル作成
 header = ['店名', 'URL']
